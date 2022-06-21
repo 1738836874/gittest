c79f949988d3e56423f6213ead6e29778eb830f7
lumo <lumo@123.com>
2022-06-21 15:10:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lumo
+ * @create 2022-06-21 15:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
